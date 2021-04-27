d4198fad93cb62a9ebef8659781a853f5565701d
Parikshit5504 <desaip932@gmail.com>
2021-04-27 09:09:21 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
+
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'APICALLING';
-}
+
+
+
+  })
+  export class AppComponent {
+  apiUrl = 'http://jsonplaceholder.typicode.com/users';
+  apiData: any;
+  
+  
+  constructor(private http: HttpClient){
+  }
+  
+  ngOnInit(){
+  this.http.get(this.apiUrl).subscribe((data)=>{
+  console.warn(data)
+  this.apiData=data;
+  })
+  }
+  }
